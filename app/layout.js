4bfa7a69b1973cfc0c5625293dc78384cc8c89dc
Shivam Raj <shivam@example.com>
2025-06-25 08:59:41 +0530

    sec

--- app/layout.js
+++ app/layout.js
@@ -37,7 +37,7 @@ export default function RootLayout({ children }) {
 
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Copyright@2025 PrepAI</p>
+                <p>Copyright@2025 Careerway.AI</p>
               </div>
             </footer>
           </ThemeProvider>
